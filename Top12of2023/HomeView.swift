1b630641d28c3b6bbb540db652bde82eff19cf1b
Mack Slevin <mack.slevin@gmail.com>
2024-04-05 18:03:02 -0700

    Add basic structiure to home view

--- Top12of2023/HomeView.swift
+++ Top12of2023/HomeView.swift
@@ -1,8 +1,28 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var top12: [GoodAlbum] = GoodAlbum.top12of2023
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            
+            VStack {
+                Text("A list of the twelve best albums released in the calendar year 2023, as determined by common sense.")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom)
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 14) {
+                        ForEach(top12) { album in
+                            Text(album.title)
+                        }
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Top 12 Albums of 2023")
+        }
     }
 }
 
