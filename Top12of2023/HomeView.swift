dfe3ef78cee9ef630f1be2cbcfe2baf08382e629
Mack Slevin <mack.slevin@gmail.com>
2024-04-05 17:38:08 -0700

    Remove some Xcode boilerplate

---
+++ Top12of2023/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Top12of2023
+//
+//  Created by Mack Slevin on 4/5/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
